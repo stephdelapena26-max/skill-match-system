94dab7bbecff5c4f7406e7782231fde9bb3b5df2
stephdelapena26-max <stephdelapena26@gmail.com>
2026-05-29 12:37:07 +0800

    m

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const app = express();
    DATABASE CONNECTION
 ========================================= */
 
+import pg from "pg";
+const { Pool } = pg;
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 /* =========================================
